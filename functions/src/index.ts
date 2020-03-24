a73c568ff2f92adb1c96a582d6af07c862b8420c
Lefras Coetzee <lefras.coetzee@gmail.com>
2020-03-24 00:24:14 +0000

    Functions PubSub Credentials Fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,3 @@
-
-import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import * as AddMessage from './add-message';
 import * as PlaycricketMatchDetailImport from './playcricket-MatchDetailImport';
@@ -9,10 +7,25 @@ import * as emailSend from './emailResults/index';
 import * as phoneAuth from './navestockSMSAuth/index';
 import * as AddMessagePubSub from './add-message-pubsub';
 import * as PlaycricketGetMatchListPubSub from './playcricket-getMatchList-pubsub';
+import * as serviceAccount from './environments/navestock-website-04b2617e4f2a';
 
-admin.initializeApp(
-    functions.config().firebase
-    );
+
+const authparams = {
+    type: serviceAccount.firebaseAuthData.type, 
+    projectId: serviceAccount.firebaseAuthData.project_id,
+    privateKeyId: serviceAccount.firebaseAuthData.private_key_id,
+    privateKey: serviceAccount.firebaseAuthData.private_key,
+    clientEmail: serviceAccount.firebaseAuthData.client_email,
+    clientId: serviceAccount.firebaseAuthData.client_id,
+    authUri: serviceAccount.firebaseAuthData.auth_uri,
+    tokenUri: serviceAccount.firebaseAuthData.token_uri,
+    authProviderX509CertUrl: serviceAccount.firebaseAuthData.auth_provider_x509_cert_url,
+    clientC509CertUrl: serviceAccount.firebaseAuthData.client_x509_cert_url
+  }
+admin.initializeApp({
+    credential: admin.credential.cert(authparams),
+    });
+
 
 export const addMessage = AddMessage.helloNavestock;
 export const playcricketMatchDetailImport = PlaycricketMatchDetailImport.listener;
